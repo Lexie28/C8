f7e042a6d259311a6ae92216dbda9278f437341d
alexandra <alexandradahlberg1@gmail.com>
2023-04-03 11:50:19 +0200

    singular server

--- server/server.js
+++ server/server.js
@@ -97,7 +97,7 @@ app.patch('/transactions/:id', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-});*/
+});
 
 
 
