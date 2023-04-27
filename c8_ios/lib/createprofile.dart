0be53383dbb9d5f375b6e01b3e6a15374d45af4c
Simon <90696241+sier8161@users.noreply.github.com>
2023-04-27 15:32:05 +0200

    Upload KANSKE fungerar, men det är något fuffens med context

--- c8_ios/lib/createprofile.dart
+++ c8_ios/lib/createprofile.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'dart:io';
 import 'api.dart';
 import 'main.dart';
 
@@ -18,6 +21,103 @@ class _CreateProfileState extends State<CreateProfile> {
   String _name = "";
   String _location = "";
   String _phone = "";
+  File? _image;
+
+  Future getImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+
+      //final imageTemporary = File(image.path);
+      final imagePermanent = await saveFilePermanently(image.path);
+
+      setState(() {
+        _image = imagePermanent;
+      });
+    } catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future<File> saveFilePermanently(String imagePath) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final name = basename(imagePath);
+    final image = File('${directory.path}/$name');
+
+    return File(imagePath).copy(image.path);
+  }
+
+  //Någon variabel som håller bilden kanske
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create Profile"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Name"),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return "Please enter your name";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _name = value!;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Location"),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return "Please enter your location";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _location = value!;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Phone Number"),
+                  keyboardType: TextInputType.phone,
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return "Please enter your phone number";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _phone = value!;
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _submitForm,
+                  child: Text("Create Profile"),
+                ),
+                CustomButton(
+                    title: 'Pick from Gallery',
+                    icon: Icons.image_outlined,
+                    onClick: () => getImage(ImageSource.gallery)),
+                CustomButton(
+                    title: 'Pick from Camera',
+                    icon: Icons.camera,
+                    onClick: () => getImage(ImageSource.camera))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   void _submitForm() async {
     _formKey.currentState!.save();
@@ -34,80 +134,36 @@ class _CreateProfileState extends State<CreateProfile> {
           'location': _location,
           'name': _name,
           'phone_number': _phone,
-          'profile_picture_path': null,
+          'profile_picture_path': '$userId.png',
         }));
 
     if (response.statusCode == 200) {
       // registration successful, navigate to main page
-      Navigator.pushReplacement(context,
+      final future = Navigator.pushReplacement(
+          context, //<----ERROR HÄR!!! wtf?!?!?
           MaterialPageRoute(builder: (context) => MyBottomNavigationbar()));
     } else {
       print("registration failed!");
     }
   }
-
-  //Någon variabel som håller bilden kanske
-  @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Create Profile"),
-    ),
-    body: SingleChildScrollView(
-      child: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: "Name"),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return "Please enter your name";
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _name = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Location"),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return "Please enter your location";
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _location = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Phone Number"),
-                keyboardType: TextInputType.phone,
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return "Please enter your phone number";
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _phone = value!;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: Text("Create Profile"),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
 }
 
+Widget CustomButton(
+    {required String title,
+    required IconData icon,
+    required VoidCallback onClick}) {
+  return Container(
+    width: 280,
+    child: ElevatedButton(
+        onPressed: onClick,
+        child: Row(
+          children: [
+            Icon(icon),
+            SizedBox(
+              width: 20,
+            ),
+            Text(title),
+          ],
+        )),
+  );
 }
