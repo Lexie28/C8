831044c786bbfd791f6d88f52aa7ae5b6f4f0c1d
Lexie28 <98817812+Lexie28@users.noreply.github.com>
2023-04-27 13:39:47 +0200

    hej

--- c8_ios/lib/api.dart
+++ c8_ios/lib/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  String _apiHost = 'http://192.168.0.104:3000'; // the string you want to return
+  String _apiHost = 'http://130.243.236.53:3000'; // the string you want to return
   String getApiHost() {
     return _apiHost;
   }
