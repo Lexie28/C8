2e351e0b628f511c116986341611d941324c9989
Simon <90696241+sier8161@users.noreply.github.com>
2023-04-28 16:02:36 +0200

    Lokal API-host

--- c8_ios/lib/specificitem.dart
+++ c8_ios/lib/specificitem.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
+import 'package:c8_ios/createbid.dart';
 import 'package:c8_ios/yourProduct.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:web_socket_channel/status.dart';
 import 'api.dart';
 import 'otherProfile.dart';
-import 'createBid.dart';
+
 
 class ListingDetailPage extends StatefulWidget {
   final String listingId;
