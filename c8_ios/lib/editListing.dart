c96c9ecf9e9b5f0ac28e40848f5ba8c5290bb13c
Simon <90696241+sier8161@users.noreply.github.com>
2023-05-05 13:35:29 +0200

    editListing mycket funktionell :)

--- c8_ios/lib/editListing.dart
+++ c8_ios/lib/editListing.dart
@@ -1,11 +1,36 @@
 import 'dart:convert';
-//import 'dart:io';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:simple_s3/simple_s3.dart';
 import 'api.dart';
 import 'main.dart';
 import 'package:path/path.dart' as p;
+
+SimpleS3 _simpleS3 = SimpleS3();
+Future<String?> _upload(File? fileToUpload) async {
+  String? result;
+  if (result == null) {
+    try {
+      result = await _simpleS3.uploadFile(
+        fileToUpload!,
+        'circle8',
+        'eu-north-1:c6a2d96e-f475-42b0-a949-f8c5f98a4b9b',
+        AWSRegions.euNorth1,
+        debugLog: true,
+        s3FolderPath: "",
+        accessControl: S3AccessControl.publicReadWrite,
+      );
+      result = p.basename(fileToUpload.path);
+    } catch (e) {
+      print(e);
+    }
+  }
+  return result;
+}
 
 class EditListing extends StatefulWidget {
   const EditListing({super.key, required this.itemId});
@@ -37,11 +62,19 @@ class _EditListingState extends State<EditListing> {
     'Sports'
   ];
 
-  Future<void> changeTitle() async {
-    print('New product title: ${_titleController.text}');
+  File? _image;
+  bool imagePicked = false;
 
+  Future<void> _submitForm() async {
+    _formKey.currentState!.save();
+    final String? uploadedImageName;
+
+    if (imagePicked) {
+      uploadedImageName = await _upload(_image);
+    } else {
+      uploadedImageName = 'noImage.jpg';
+    }
     if (_formKey.currentState!.validate()) {
-      //final url = Uri.parse('${_api.getApiHost()}/listing/:id');
       final url = Uri.parse('${_api.getApiHost()}/listing/${widget.itemId}');
 
       final headers = {'Content-Type': 'application/json'};
@@ -49,7 +82,7 @@ class _EditListingState extends State<EditListing> {
         'name': _titleController.text,
         'description': _descController.text,
         'category': _catController.text,
-        'image_path': null,
+        'image_path': uploadedImageName,
       };
       final jsonBody = json.encode(body);
       final response = await http.patch(url, headers: headers, body: jsonBody);
@@ -70,6 +103,33 @@ class _EditListingState extends State<EditListing> {
     }
   }
 
+  Future getImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+
+      final imagePermanent = await saveFilePermanently(image.path);
+
+      setState(() {
+        _image = imagePermanent;
+      });
+    } catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future<File> saveFilePermanently(String imagePath) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final ext = p.extension(imagePath);
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getString('uid');
+    final image = File('${directory.path}/$userId$ext');
+
+    imagePicked = true;
+
+    return File(imagePath).copy(image.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -85,17 +145,21 @@ class _EditListingState extends State<EditListing> {
               // Profile picture
               GestureDetector(
                 onTap: () {
-                  ; //TODO här
+                  getImage(ImageSource.gallery);
                 },
                 child: Container(
                   margin:
                       EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(120.0),
-                    child: Image.asset(
-                      'images/apple.jpg',
-                      fit: BoxFit.cover,
-                    ),
+                    child: imagePicked
+                        ? Image.file(
+                            _image!,
+                            fit: BoxFit.cover,
+                          )
+                        : Image.asset(
+                            'images/noImage.jpg',
+                          ),
                   ),
                 ),
               ),
@@ -155,7 +219,7 @@ class _EditListingState extends State<EditListing> {
                 ),
                 child: ElevatedButton(
                   onPressed: () {
-                    changeTitle();
+                    _submitForm();
                   },
                   child: Text('Save Changes'),
                 ),
